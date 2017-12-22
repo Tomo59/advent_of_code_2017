c3d73ff4045eca9338f09f30d73940ad678367cf
Thomas Gambier <thomas.gambier@gmail.com>
2017-12-22 08:55:53 +0100

    No enum for speed

--- day22/part2/main.py
+++ day22/part2/main.py
@@ -1,52 +1,52 @@
 #!/usr/bin/env python3
 
-from enum import Enum
-class Dir(Enum):
-  UP = 0
-  RIGHT = 1
-  DOWN = 2
-  LEFT = 3
-
-class State(Enum):
-  CLEAN = 0
-  WEAKENED = 1
-  INFECTED = 2
-  FLAGGED = 3
-
+#from enum import Enum
+#class Dir(Enum):
+#  UP = 0
+#  RIGHT = 1
+#  DOWN = 2
+#  LEFT = 3
+#
+#class State(Enum):
+#  CLEAN = 0
+#  WEAKENED = 1
+#  INFECTED = 2
+#  FLAGGED = 3
+#
 def left(d):
-  return Dir((d.value+4-1)%4)
+  return (d+4-1)%4
 
 def right(d):
-  return Dir((d.value+1)%4)
+  return (d+1)%4
 
 def reverse(d):
-  return Dir((d.value+2)%4)
+  return (d+2)%4
 
 def state(y, x):
   global infected
   if y not in infected.keys():
     infected[y] = dict()
   if x not in infected[y].keys():
-    infected[y][x] = State.CLEAN
+    infected[y][x] = 0
   return infected[y][x]
 
 def move(y, x, direction):
   #print("we are at position [{}][{}] and facing {}".format(y, x, direction.name))
   global result
-  if state(y, x) == State.INFECTED:
+  if state(y, x) == 2: #State.INFECTED
     d = right(direction)
-  elif state(y, x) == State.CLEAN:
+  elif state(y, x) == 0: #State.CLEAN
     d = left(direction)
-  elif state(y, x) == State.FLAGGED:
+  elif state(y, x) == 3: #State.FLAGGED
     d = reverse(direction)
   else: # WEAKENED
     d = direction
     result += 1
-  infected[y][x] = State((infected[y][x].value+1)%4)
-  if d == Dir.UP: y -= 1
-  elif d == Dir.DOWN: y += 1
-  elif d == Dir.RIGHT: x += 1
-  elif d == Dir.LEFT: x -= 1
+  infected[y][x] = (infected[y][x]+1)%4
+  if d == 0 : y -= 1
+  elif d == 2 : y += 1
+  elif d == 1 : x += 1
+  elif d == 3 : x -= 1
   else:
     print("Error bad direction {}".format(d))
     quit()
@@ -58,20 +58,20 @@ for i,line in enumerate(f):
   infected[i] = dict()
   for j,d in enumerate(list(line)[:-1]):
     if d == '#':
-      infected[i][j] = State.INFECTED
+      infected[i][j] = 2 # State.INFECTED
     else:
-      infected[i][j] = State.CLEAN
+      infected[i][j] = 0 # State.CLEAN
 
 #print(infected)
 
 result = 0
 y = (len(infected)-1)//2
 x = (len(infected[0])-1)//2
-direction = Dir.UP
+direction = 0
 
 for i in range(10000000):
   y, x, direction = move(y, x, direction)
-  if (i%10000) == 0: print("i = {} and for now result is {}".format(i, result))
+  #if (i%10000) == 0: print("i = {} and for now result is {}".format(i, result))
 
 
 print("result is {}".format(result))
